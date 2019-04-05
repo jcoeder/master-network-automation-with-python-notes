013e629b9a33d6eb21fa91b3f4641efcfe444fd9
Justin Oeder <jcoeder@gmail.com>
2019-04-05 18:47:17 -0400

    Create telnet-add-loopback-0-9-list.py

---
+++ telnet-add-loopback-0-9-list.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import telnetlib
+import time
+
+hosts = ['172.31.33.201', '172.31.33.202', '172.31.33.203']
+user = 'cisco'
+password = 'cisco'
+enable = None
+
+for host in hosts:
+    tn = telnetlib.Telnet(host)
+
+    tn.read_until('Username: ')
+    tn.write(user + '\n')
+    if password:
+        tn.read_until('Password: ')
+        tn.write(password + '\n')
+
+    tn.write('enable\n')
+    tn.write('terminal length 0\n')
+
+    if enable is not None:
+        tn.read_until('Password: ')
+        tn.write(enable + '\n')
+        tn.read_until('#')
+    else:
+        tn.read_until('#')
+
+    tn.write('configure terminal\n')
+    tn.read_until('(config)#')
+
+    for i in range(10):
+        tn.write('interface loopback ' + str(i) + '\n')
+        tn.write('ip address 1.1.1.' + str(i) + ' 255.255.255.255\n')
+        time.sleep(.2)
+
+    tn.write('exit\n')
+    tn.write('write memory\n')
