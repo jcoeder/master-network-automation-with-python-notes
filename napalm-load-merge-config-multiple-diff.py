2a177bad9149dff8655f3729e230290a4def81d1
Justin Oeder <jcoeder@gmail.com>
2019-04-17 22:42:13 -0400

    more napalm

--- napalm-load-merge-config-multiple-diff.py
+++ napalm-load-merge-config-multiple-diff.py
@@ -1,3 +1,4 @@
+import json
 from napalm import get_network_driver
 
 driver = get_network_driver('ios')
@@ -5,6 +6,16 @@ iosvl3 = driver('172.31.33.201', 'cisco', 'cisco')
 iosvl3.open()
 
 iosvl3.load_merge_candidate(filename='access-list.cfg')
+
+diffs = iosvl3.compare_config()
+if len(diffs) > 0:
+    print(diffs)
+    iosvl3.commit_config()
+else:
+    print('No changes required.')
+    iosvl3.discard_config()
+
+ iosvl3.load_merge_candidate(filename='ospf.cfg')
 
 diffs = iosvl3.compare_config()
 if len(diffs) > 0:
